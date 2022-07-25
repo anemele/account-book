d049859b032481b0131985edc2021fb8c94b7f2c
Jason <anemele@outlook.com>
2022-07-25 23:12:27 +0800

    fix: 1. background of save button in CreateActivity 2. type of log in input 3. logic of log in

--- app/src/main/java/com/js/ab/LoginActivity.java
+++ app/src/main/java/com/js/ab/LoginActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    private final String account = "admin";
+    private final String password = "123456";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +24,25 @@ public class LoginActivity extends AppCompatActivity {
         CheckBox checkBox = findViewById(R.id.login_btn_agree);
 
         btnLogIn.setOnClickListener(v -> {
-            if (etAccount.getText().toString().equals("admin") &&
-                    etPassword.getText().toString().equals("123456")) {
-                if (checkBox.isChecked()) {
-                    Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(this, "请先同意协议", Toast.LENGTH_SHORT).show();
-                }
+            if (etAccount.getText().toString().isEmpty()) {
+                Toast.makeText(this, "账号不能为空", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (etPassword.getText().toString().isEmpty()) {
+                Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!checkBox.isChecked()) {
+                Toast.makeText(this, "请先同意协议", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (etAccount.getText().toString().equals(account) &&
+                    etPassword.getText().toString().equals(password)) {
+                Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
             } else {
-                Toast.makeText(this, "密码或账号错误", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "账号或密码错误", Toast.LENGTH_SHORT).show();
             }
         });
     }
